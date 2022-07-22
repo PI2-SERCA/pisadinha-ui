880f494adb54812bae70afe8806e569fcf919077
igor-paiva <igorbatistapaiva@outlook.com>
2022-07-21 23:32:40 -0300

    Redirect to room page on room template click

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,47 +8,36 @@ import {
   Container,
 } from '@material-ui/core';
 
+import { useHistory } from 'react-router-dom';
 import { ItemCard } from '../../layout';
 
-export const Home: React.FC = () => (
-  <Container maxWidth="lg">
-    <Paper square>
-      <Tabs
-        value={0}
-        indicatorColor="primary"
-        textColor="primary"
-      >
-        <Tab label="Cômodos" />
-        <Tab label="Cortes" />
-      </Tabs>
-    </Paper>
+export const Home: React.FC = () => {
+  const history = useHistory();
 
-    <Box style={{ marginTop: 16, marginLeft: 16 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <ItemCard />
+  return (
+    <Container maxWidth="lg">
+      <Paper square>
+        <Tabs
+          value={0}
+          indicatorColor="primary"
+          textColor="primary"
+        >
+          <Tab label="Cômodos" />
+          <Tab label="Cortes" />
+        </Tabs>
+      </Paper>
+
+      <Box style={{ marginTop: 16, marginLeft: 16 }}>
+        <Grid container spacing={2}>
+          {Array.from(Array(10).keys()).map((idx) => (
+            <Grid item key={idx} xs={12} sm={6} md={4} lg={2} onClick={() => history.push('/room')}>
+              <ItemCard />
+            </Grid>
+          ))}
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <ItemCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <ItemCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <ItemCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <ItemCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <ItemCard />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2}>
-          <ItemCard />
-        </Grid>
-      </Grid>
-    </Box>
-  </Container>
-);
+      </Box>
+    </Container>
+  );
+};
 
 export default Home;
