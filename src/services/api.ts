b6793ce44e4a4958e36a93ab8490f6db4f833282
igor-paiva <igorbatistapaiva@outlook.com>
2022-08-26 15:55:40 -0300

    Creates API adapter

---
+++ src/services/api.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+const CONFIG: AxiosRequestConfig = {
+  timeout: parseInt(process.env.REACT_APP_PISADINHA_TIMEOUT || '5000', 10),
+  baseURL: `${process.env.REACT_APP_PISEIRO_API_URL}/`,
+  headers: {
+    Accept: 'application/json',
+  },
+};
+
+export default class APIAdapter {
+  private instance: AxiosInstance;
+
+  constructor() {
+    this.instance = axios.create(CONFIG);
+  }
+
+  async get(path: string, config?: AxiosRequestConfig) {
+    const res = await this.instance.get(path, config);
+
+    return res.data.results || res.data;
+  }
+
+  async post(
+    path: string,
+    data?: Record<string, never>,
+    config?: AxiosRequestConfig
+  ) {
+    const res = await this.instance.post(path, data, config);
+    return res.data.results || res;
+  }
+
+  async patch(
+    path: string,
+    data?: Record<string, never>,
+    config?: AxiosRequestConfig
+  ) {
+    const res = await this.instance.patch(path, data, config);
+
+    return res.data.results || res;
+  }
+
+  async delete(path: string, config?: AxiosRequestConfig) {
+    const res = await this.instance.delete(path, config);
+
+    return res.data.results || res;
+  }
+}
